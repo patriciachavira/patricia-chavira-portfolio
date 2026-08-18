4520651d9a0d4342494e0f45a6e279ecdf4fd0c5
patriciachavira <patriciacfch@gmail.com>
2026-08-17 22:34:57 -0600

    Update routes.ts

--- src/routes.ts
+++ src/routes.ts
@@ -1,34 +1,33 @@
-import { createBrowserRouter } from 'react-router'
-import MainHome from './pages/MainHome'
-import Home from './pages/Home'
-import CaseStudy01 from './pages/CaseStudy01'
-import CaseStudy02 from './pages/CaseStudy02'
-import CaseStudy03 from './pages/CaseStudy03'
-import CaseStudy04 from './pages/CaseStudy04'
+import { createBrowserRouter } from 'react-router';
+import Home from './pages/Home';
+import CaseStudy01 from './pages/CaseStudy01';
+import CaseStudy02 from './pages/CaseStudy02';
+import CaseStudy03 from './pages/CaseStudy03';
+import CaseStudy04 from './pages/CaseStudy04';
 
 export const router = createBrowserRouter([
   {
     path: '/',
-    Component: MainHome,
+    element: <Home />,
   },
   {
-    path: '/work',
-    Component: Home,
+    path: '/case-study-1',
+    element: <CaseStudy01 />,
   },
   {
-    path: '/case-study-01',
-    Component: CaseStudy01,
+    path: '/case-study-2',
+    element: <CaseStudy02 />,
   },
   {
-    path: '/case-study-02',
-    Component: CaseStudy02,
+    path: '/case-study-3',
+    element: <CaseStudy03 />,
   },
   {
-    path: '/case-study-03',
-    Component: CaseStudy03,
+    path: '/case-study-4',
+    element: <CaseStudy04 />,
   },
   {
-    path: '/case-study-04',
-    Component: CaseStudy04,
+    path: '*',
+    element: <Home />,
   },
-])
+]);
