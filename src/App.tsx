a06bd2e5716b531340557c705a387e98c3a72b9b
patriciachavira <patriciacfch@gmail.com>
2026-08-17 22:37:41 -0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,21 @@
-import { RouterProvider } from 'react-router'
-import { router } from './routes'
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import CaseStudy01 from './pages/CaseStudy01';
+import CaseStudy02 from './pages/CaseStudy02';
+import CaseStudy03 from './pages/CaseStudy03';
+import CaseStudy04 from './pages/CaseStudy04';
 
 export default function App() {
-  return <RouterProvider router={router} />
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/case-study-1" element={<CaseStudy01 />} />
+        <Route path="/case-study-2" element={<CaseStudy02 />} />
+        <Route path="/case-study-3" element={<CaseStudy03 />} />
+        <Route path="/case-study-4" element={<CaseStudy04 />} />
+        <Route path="*" element={<Home />} />
+      </Routes>
+    </HashRouter>
+  );
 }
